4a4c50662c2cb95490fc7f1a50e0d773734b9860
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-01 23:34:37 +0530

    Time: 0 ms (100%), Space: 30.5 MB (18.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k%n;
+        k %= n;
         vector<int> temp = nums;
-        int t = n-k, f = 0;
         for(int i=0; i<n; i++) {
-            if(t<n) {
-                nums[i] = temp[t];
-                t++;
-            }
-            else {
-                nums[i] = temp[f];
-                f++;
-            }
+            nums[i] = temp[(i+n-k)%n];
         }
     }
 };
