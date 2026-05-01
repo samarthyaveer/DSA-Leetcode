ee84488c498716e0d2b22a4686f69895e2cab89f
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-02 00:10:36 +0530

    Time: 0 ms (100%), Space: 23.8 MB (54.55%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int z = 0;
+        for(int i=0; i<nums.size(); i++)    if(nums[i] != 0)    swap(nums[i], nums[z++]);
+    }
+};
