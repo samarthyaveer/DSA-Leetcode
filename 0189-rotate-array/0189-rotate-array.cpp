01ab80434ec1298fbf744e87f4be5bac7ae31363
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-01 23:26:54 +0530

    Time: 0 ms (100%), Space: 30.5 MB (12.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,8 +4,16 @@ public:
         int n = nums.size();
         k = k%n;
         vector<int> temp = nums;
+        int t = n-k, f = 0;
         for(int i=0; i<n; i++) {
-            nums[(i+k)%n] = temp[i];
+            if(t<n) {
+                nums[i] = temp[t];
+                t++;
+            }
+            else {
+                nums[i] = temp[f];
+                f++;
+            }
         }
     }
 };
