ee9844ef80dbdf60f32d844f900c62cbe879ec12
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-03 11:37:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.87%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string t = s;
-        int n = t.length();
-        if(goal.length() != n) return false;
-        for(int i=0; i<n; i++) {
-            char temp = t[0];
-            for(int j=0; j<n-1; j++) {
-                t[j] = t[j+1];
+        int n = s.size();
+        int m = goal.size();
+
+        if (n != m) return false;
+
+        for (int i=0; i<n; i++) {
+            char first = s[0];
+            for (int j=0; j<n-1; j++) {
+                s[j] = s[j+1];
             }
-            t[n-1] = temp;
-            if(t == goal) return true;
+            
+            s[n - 1] = first;
+            if (s == goal) return true;
         }
         return false;
     }
