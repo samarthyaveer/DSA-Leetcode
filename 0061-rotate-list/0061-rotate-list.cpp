6e20e6fe15e740b79223d25c2a13514786fd20cf
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-05 22:20:03 +0530

    Time: 0 ms (100%), Space: 17.2 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,23 +11,27 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL) return head;
+        if(!head || !head->next) return head;
         vector<int> listArr;
         ListNode* temp = head;
+
         while(temp) {
             listArr.push_back(temp->val);
             temp = temp->next;
         }
+
         int n = listArr.size();
         k = k%n;
-        reverse(listArr.begin(), listArr.end());
-        reverse(listArr.begin(), listArr.begin()+k);
-        reverse(listArr.begin()+k, listArr.end());
+        vector<int> rotatedArr(n);
+
+        for(int i=0; i<n; i++) {
+            rotatedArr[(i+k) % n] = listArr[i];
+        }
+
         temp = head;
         int i=0;
         while(temp) {
-            temp->val = listArr[i];
-            i++;
+            temp->val = rotatedArr[i++];
             temp = temp->next;
         }
         return head;
