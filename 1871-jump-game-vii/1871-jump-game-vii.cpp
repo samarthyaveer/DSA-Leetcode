5cb2393b11a0cb174c6588f3e2d4608c73918187
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-05-25 23:40:43 +0530

    Time: 13 ms (38.98%), Space: 24.6 MB (45.87%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        queue<int> q;
+        vector<bool> visited(n, false);
+        q.push(0);
+        visited[0] = true;
+        int far = 0;
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+            if (i == n - 1) return true;
+            int start = max(i + minJump, far + 1);
+            int end = min(i + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+                if (s[j] == '0' && !visited[j]) {
+                    visited[j] = true;
+                    q.push(j);
+                }
+            }
+            far = max(far, end);
+        }
+        return false;
+    }
+};
