c5fad4fbf8f81be4fa58e7f520a6043fecb9260f
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-07-02 18:44:55 +0530

    Time: 0 ms (100%), Space: 15.2 MB (8.98%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -9,27 +9,18 @@ public:
         reverse(mat.begin(), mat.end());
         return mat;
     }
-
-    bool isEqual(vector<vector<int>>& mat, vector<vector<int>>& target, int n) {
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(mat[i][j] != target[i][j]) return false;
-            }
-        }
-        return true;
-    }
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n = mat.size();
-        if(isEqual(mat, target, n)) return true;
+        if(mat==target) return true;
 
         mat = rotate(mat, n);
-        if(isEqual(mat, target, n)) return true;
+        if(mat==target) return true;
 
         mat = rotate(mat, n);
-        if(isEqual(mat, target, n)) return true;
+        if(mat==target) return true;
 
         mat = rotate(mat, n);
-        if(isEqual(mat, target, n)) return true;
+        if(mat==target) return true;
 
         return false;
     }
