b355f76344b4fa5ee5646191897ba6eadc2eb60c
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-07-07 11:49:41 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.99%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
-        int i = n - 1;
+        int i = s.length() - 1;
         string ans = "";
-        
-        while (i >= 0) {
-            // 1. Skip any trailing or multiple spaces
-            while (i >= 0 && s[i] == ' ') {
-                i--;
-            }
-            
-            // If i drops below 0 after skipping spaces, we are completely done
-            if (i < 0) break;
-            
-            // 2. We hit a letter! Mark this as the end of the word (j)
+        while(i >= 0) {
+            while(i >= 0 && s[i]==' ') i--;
+            if(i < 0) break;
             int j = i;
-            
-            // 3. Keep moving left until we hit a space (finding the start of the word)
-            while (i >= 0 && s[i] != ' ') {
-                i--;
-            }
-            
-            // 4. Extract the word! 
-            // It starts at i + 1, and its length is j - i
-            if (!ans.empty()) {
-                ans += " "; // Only add a space before the word if ans isn't empty
-            }
-            ans += s.substr(i + 1, j - i);
+            while(i >= 0 && s[i] != ' ') i--;
+            if(!empty(ans)) ans += ' ';
+            ans += s.substr(i+1, j-i);
         }
-        
         return ans;
     }
 };
