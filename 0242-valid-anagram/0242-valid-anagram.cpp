00c5c0e9679dd2bb4e42a6eff25c97c29440504d
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-07-09 20:03:00 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.7%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+
+        for(char c : s) mp[c]++;
+        for(char c : t) mp[c]--;
+        for(auto it : mp) {
+            if(it.second != 0) return false;
+        }
+        return true;
+    }
+};
