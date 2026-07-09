27bd7b41f951a92b29f0dee769a4c0743e652642
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-07-09 19:32:02 +0530

    Time: 0 ms (100%), Space: 11.6 MB (93.61%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        int m = strs[0].size();
-        if(n <= 1) return strs[0];
-        string ans = "";
-
-        for(int i=0; i<m; i++) {
-            char ref = strs[0][i];
-            for(int j=1; j<n; j++) {
-                if(i >= strs[j].size() || strs[j][i] != ref) return ans;
+        int n = strs.size(); 
+        if(n == 1) return strs[0];
+        int m = strs[0].size(); 
+        
+        for(int i = 0; i < m; i++) {
+            char ref = strs[0][i]; 
+            for(int j = 1; j < n; j++) {
+                if(i >= strs[j].size() || strs[j][i] != ref) {
+                    return strs[0].substr(0, i); 
+                }
             }
-            ans += ref;
         }
-        return ans;
+        return strs[0];
     }
 };
